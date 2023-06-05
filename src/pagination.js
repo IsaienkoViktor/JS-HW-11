9f8ea851ced7c8fca978f940f7708319c1db6729
IsaienkoViktor <isaenkov1986@gmail.com>
2023-06-05 15:38:28 +0300

    add

--- src/pagination.js
+++ src/pagination.js
@@ -7,15 +7,8 @@ const jsonPixabayApi = new JsonPixabayApi();
 
 const searchForm = document.querySelector('.search-form');
 const galleryListEl = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load');
+const loadMoreBtn = document.querySelector('.load-more');
 
-const onloadMoreclickFoo = event => {
-    console.log('hello');
-}
-
-loadMoreBtn.addEventListener('click', onloadMoreclickFoo);
-
-console.log(loadMoreBtn);
 
 //fetch arr from request + render images to galleryListEl
 
@@ -29,3 +22,17 @@ jsonPixabayApi
         console.log(err);
     });
     
+const onloadMoreClickFoo = event => {   
+    jsonPixabayApi.page += 1;
+    jsonPixabayApi.fetchFromAPi()
+        .then(images => {
+            galleryListEl.insertAdjacentHTML('beforeend', createImgCard(images.hits))
+        console.log(images);
+        })
+        .catch(err => {
+            console.log(err);
+    })
+}
+loadMoreBtn.addEventListener('click', onloadMoreClickFoo);
+
+console.log(loadMoreBtn);
