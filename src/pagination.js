a3c7ac3780dfe0284f8bef8f109bdaf9e2bc19bb
IsaienkoViktor <isaenkov1986@gmail.com>
2023-06-11 17:00:43 +0300

    add

--- src/pagination.js
+++ src/pagination.js
@@ -96,6 +96,15 @@ loadMoreBtn.addEventListener('click', async (e) =>{
     })
 })
 
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
 
 
 
